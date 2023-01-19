ca0b150070042103731b22445c86a2fe60722697
JONATANZCH <jonatanzch@gmail.com>
2023-01-19 16:58:13 -0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <h1 className="font-black text-5xl text-center md:w-2/4 mx-auto">
+            Seguimiento Pacientes {""} 
+            <span className="text-rose-900">Veterinaria</span>
+        </h1>
+    )
+}
+
+export default Header;
